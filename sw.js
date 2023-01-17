4d6283f225932bc8633bf1371da2e7b281708b6c
Ronk12Haldankar <raunak.infipreintern@gmail.com>
2023-01-17 18:00:57 +0530

    changes

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
-      '/pwa-examples/a2hs/',
-      '/pwa-examples/a2hs/index.html',
-      '/pwa-examples/a2hs/index.js',
-      '/pwa-examples/a2hs/style.css',
-      '/pwa-examples/a2hs/images/banner-bg.jpg'
+      'https://ronk12rk.github.io/hostRonk/',
+      'https://ronk12rk.github.io/hostRonk/index.html',
+      'https://ronk12rk.github.io/hostRonk/index.js',
+      'https://ronk12rk.github.io/hostRonk/style.css',
+      'https://ronk12rk.github.io/hostRonk/images/banner-bg.jpg'
       // '/pwa-examples/a2hs/images/fox1.jpg',
       // '/pwa-examples/a2hs/images/fox2.jpg',
       // '/pwa-examples/a2hs/images/fox3.jpg',
